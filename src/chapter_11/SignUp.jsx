6b0dc4cad6609bffb512b0a6f1769b809944f5e1
seungcheo <ktj9266@naver.com>
2024-05-29 16:06:43 +0900

    20240529

--- src/chapter_11/SignUp.jsx
+++ src/chapter_11/SignUp.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 export default function SignUp() {
     const [name, setName] = useState()
     const [gender, setGender] = useState('남자')
+    const [document, setDocument] = useState()
 
     const handleChangeName = (e) => {
         setName(e.target.value)
@@ -10,8 +11,11 @@ export default function SignUp() {
     const handleChangeGender = (e) => {
         setGender(e.target.value)
     }
+    const handleChangeDocument = (e) => {
+        setDocument(e.target.value)
+    }
     const handleSubmit = (e) => {
-        alert(`name: ${name}, gender: ${gender}`)
+        alert(`name: ${name}, gender: ${gender} document: ${document}`)
         e.preventDefault()
     }
     return(
@@ -21,6 +25,23 @@ export default function SignUp() {
                 <input type="text" value={name} onChange={handleChangeName} placeholder="이름을 입력해주세요"/>
 
             </label>
+            <br></br>
+            <label>
+                gender:
+                <select value={name} onChange={handleChangeGender} >
+                <option value="남자">남자</option>
+                <option value="여자">여자</option>
+                </select>
+
+            </label>
+            <br>
+            </br>
+            <label>
+                document:
+                <textarea value={document} onChange={handleChangeDocument} placeholder="문장을 입력해주세요."/>
+            </label>
+            <br>
+            </br>
             <button type="submit">전송</button>
         </form>
     )
